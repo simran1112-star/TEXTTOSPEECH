6c8c2a131e3602ae90b5ff1afa213d1292c9ccc9
simran arora <140587380+simran1112-star@users.noreply.github.com>
2023-09-11 11:38:57 +0530

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,33 @@
+// script.js
+
+const urlOption = document.getElementById("urlOption");
+const textOption = document.getElementById("textOption");
+const urlInputSection = document.getElementById("urlInputSection");
+const displayOptionSection = document.getElementById("displayOptionSection");
+const textInputSection = document.getElementById("textInputSection");
+const webpageContentSelector = document.getElementById("webpage-frame");
+
+urlOption.addEventListener("click", () => {
+    urlInputSection.style.display = "block";
+    displayOptionSection.style.display = "block"; // Show the display option for URL
+    textInputSection.style.display = "none";
+});
+
+textOption.addEventListener("click", () => {
+    urlInputSection.style.display = "none";
+    displayOptionSection.style.display = "none"; // Hide the display option for URL
+    textInputSection.style.display = "block";
+    webpageContentSelector.style.display = "none";
+});
+
+function loadWebpage() {
+    const urlInput = document.getElementById("urlInput").value;
+
+    setIframeSource(urlInput);
+    webpageContentSelector.style.display = "block";
+}
+
+function setIframeSource(url) {
+    var iframe = document.getElementById("webpage-frame");
+    iframe.src = url;
+}
